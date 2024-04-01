dc4961cd5ae2b7b46ac831c996f5584c63fc4336
illzie <64164811+illzie@users.noreply.github.com>
2024-04-01 17:21:50 -0600

    Update

--- eyecolor.js
+++ eyecolor.js
@@ -1,33 +1,37 @@
-let id = 1;
+let id = 1
 // sets default ID number
-let table = document.getElementById('eyeColorTable');
+let table = document.getElementById('eyeColorTable')
 // Gets table from the dom
+
+const nameInput = document.getElementById('participantName')
+const eyeColorInput = document.getElementById('eyeColor')
+
+const validationMessage = document.getElementById('validationMessage')
+
 
 document.getElementById('addToTable').addEventListener('click', () => {
 
     let row = table.insertRow(1);
-    row.setAttribute('id', `item-${id}`);
-    row.insertCell(0).innerHTML = document.getElementById('participantName').value;
-    row.insertCell(1).innerHTML = document.getElementById('eyeColor').value;
-    let actions = row.insertCell(2);
-    actions.appendChild(deleteBtn(id++));
-    document.getElementById('participantName').value = '';
+    row.setAttribute('id', `item-${id}`)
+    row.insertCell(0).innerHTML = participantName.value
+    row.insertCell(1).innerHTML = eyeColorInput.value
+    let actions = row.insertCell(2)
+    actions.appendChild(deleteBtn(id++))
+
+    participantName.value = ' ';
+    eyeColorInput.value = ' '
 
 });
 
 function deleteBtn(id) {
-    let btn = document.createElement('button');
-    btn.className = 'button-as-link text-danger';
-    btn.id = id;
-    btn.innerHTML = '<i class="fa fa-trash-o" aria-hidden="true"></i> Delete';
+    let btn = document.createElement('button')
+    btn.className = 'button-as-link text-danger'
+    btn.id = id
+    btn.innerHTML = '<i class="fa fa-trash-o" aria-hidden="true"></i> Delete'
     btn.onclick = () => {
         console.log(`Deleting row item-${id}`);
-        let deletedRow = document.getElementById(`item-${id}`);
-        deletedRow.parentNode.removeChild(deletedRow);
+        let deletedRow = document.getElementById(`item-${id}`)
+        deletedRow.parentNode.removeChild(deletedRow)
     };
     return btn;
-}
-
-function validation() {
-
 }
